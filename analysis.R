acb68aefe2751a3ee790a38bd46f54468d0d688d
robhyman89 <rdh519@york.ac.uk>
2020-10-20 13:42:36 +0100

    created dataset and summarised

---
+++ analysis.R
@@ -0,0 +1,11 @@
+# make dataset
+
+df <- data.frame(x = rnorm(50, 15, 4), 
+                 group = rep(c("group1", "group2"), each = 25))
+view(df)
+library(tidyverse)
+
+df %>% group_by(group) %>%
+  summarise(mean = mean(x),
+            n = length(n),
+            sd = sd(x))
