2e2c7299e747c2fdf322c148e07f0579396afccc
robhyman89 <rdh519@york.ac.uk>
2020-11-02 11:49:39 +0000

    tidied and more

--- analysis.R
+++ analysis.R
@@ -5,10 +5,18 @@ df <- data.frame(x = rnorm(50, 15, 4),
 view(df)
 library(tidyverse)
 
+# how to tidy data
+haff2<- chaff%>% 
+  pivot_longer(names_to = "sex", 
+               values_to = "mass",
+               cols = everything())
+
+# how to summarise data
 df %>% group_by(group) %>%
   summarise(mean = mean(x),
             n = length(n),
-            sd = sd(x))
+            sdev = sd(x))
 
+#making a plot
 df %>% ggplot(aes(x = group, y = x)) +
       geom_boxplot()
